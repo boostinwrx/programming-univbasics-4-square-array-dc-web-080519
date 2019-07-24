1ed4c6d2f74f96d3a7418a2e0d23574c4b58ccf3
ANTONIO J ABRAHAM <tony.calablava@gmail.com>
2019-07-24 00:04:24 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0
   new_array = [ ]
   while counter < array.length do
-    new_array.unshift(array[counter]
+    new_array.unshift(array[counter]**2)
     counter += 1
   end
 end
